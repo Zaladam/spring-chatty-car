9ac0d120ebc16a59918deb8add182b43deb1eb6d
adam.elmrabet <adam.elmrabet@student.vinci.be>
2022-11-29 17:22:28 +0100

    adding http response and exceptions

--- passenger/src/main/java/be/vinci/ipl/passenger/PassengersController.java
+++ passenger/src/main/java/be/vinci/ipl/passenger/PassengersController.java
@@ -2,6 +2,8 @@ package be.vinci.ipl.passenger;
 
 import be.vinci.ipl.passenger.model.Passengers;
 import be.vinci.ipl.passenger.model.Trips;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PassengersController {
@@ -19,50 +22,68 @@ public class PassengersController {
   }
 
   @GetMapping("/passengers/{trip_id}")
-  public Passengers readFromTrip(@PathVariable long trip_id){
-
-    return service.findPassengersFromTrip(trip_id);
+  public ResponseEntity<Passengers> readFromTrip(@PathVariable long trip_id){
+    Passengers passengers = service.findPassengersFromTrip(trip_id);
+    if(passengers==null)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No trip found with this ID");
+    return new ResponseEntity<>(passengers,HttpStatus.OK);
   }
 
   @DeleteMapping("/passengers/{trip_id}")
-  public void deleteAllPassengers(@PathVariable long trip_id){
-
-    service.deleteAllPassengersFromTrip(trip_id);
+  public ResponseEntity<Void> deleteAllPassengers(@PathVariable long trip_id){
+    boolean response = service.deleteAllPassengersFromTrip(trip_id);
+    if(!response)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No trip found with this ID");
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
   @GetMapping("/passengers/{user_id}")
-  public Trips readPassengerFromTrip(@PathVariable long user_id){
-
-    return service.readTripByPassengerId(user_id);
+  public ResponseEntity<Trips> readPassengerFromTrip(@PathVariable long user_id){
+    Trips trips = service.readTripByPassengerId(user_id);
+    if(trips==null)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No user found with this ID");
+    return new ResponseEntity<>(trips,HttpStatus.OK);
   }
 
   @DeleteMapping("/passengers/{user_id}")
-  public void deletePassengerFromTrip(@PathVariable long user_id){
+  public ResponseEntity<Void> deletePassengerFromTrip(@PathVariable long user_id){
+    boolean response = service.deletePassengersFromTrip(user_id);
+    if(!response)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No user found with this ID");
+    return new ResponseEntity<>(HttpStatus.OK);
 
-    service.deletePassengersFromTrip(user_id);
   }
 
   @PostMapping("/passengers/{trip_id}/{user_id}")
-  public void addPassengerFromTrip(@PathVariable long user_id,@PathVariable long trip_id){
-
-    service.addPassengerToTrip(user_id,trip_id);
-
+  public ResponseEntity<Void> addPassengerFromTrip(@PathVariable long user_id,@PathVariable long trip_id){
+    boolean response = service.addPassengerToTrip(user_id,trip_id);
+    if(!response)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Trip or user not found");
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
   @GetMapping("/passengers/{trip_id}/{user_id}")
-  public String readPassengerFromTrip(@PathVariable long user_id,@PathVariable long trip_id){
-
-    return service.readStatusFromPassenger(user_id,trip_id);
+  public ResponseEntity<String> readPassengerFromTrip(@PathVariable long user_id,@PathVariable long trip_id){
+    String status = service.readStatusFromPassenger(user_id,trip_id);
+    if(status == null)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Trip or user not found");
+    return new ResponseEntity<>(status,HttpStatus.OK);
   }
 
   @PutMapping("/passengers/{trip_id}/{user_id}")
-  public void updatePassengerStatus(@PathVariable long user_id,@PathVariable long trip_id,@RequestParam String status){
+  public ResponseEntity<Void> updatePassengerStatus(@PathVariable long user_id,@PathVariable long trip_id,@RequestParam String status){
+    boolean response = service.updatePassengerStatus(trip_id,user_id,status);
+    if(!response)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Trip or user not found");
+    return new ResponseEntity<>(HttpStatus.OK);
 
-    service.updatePassengerStatus(trip_id,user_id,status);
   }
   @DeleteMapping("/passengers/{trip_id}/{user_id}")
-  public void deletePassengerFromTrip(@PathVariable long user_id,@PathVariable long trip_id){
+  public ResponseEntity<Void> deletePassengerFromTrip(@PathVariable long user_id,@PathVariable long trip_id){
+    boolean response = service.deletePassengerFromTrip(trip_id,user_id);
+    if(!response)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Trip or user not found");
+    return new ResponseEntity<>(HttpStatus.OK);
 
-    service.deletePassengerFromTrip(trip_id,user_id);
   }
 }
--- passenger/src/main/java/be/vinci/ipl/passenger/PassengersService.java
+++ passenger/src/main/java/be/vinci/ipl/passenger/PassengersService.java
@@ -27,6 +27,8 @@ public class PassengersService {
 
   public Passengers findPassengersFromTrip(long id) {
     Iterable<Passenger> passengers = repository.findByTripId(id);
+    if(passengers == null)
+      return null;
     Passengers result = new Passengers();
 
     for(Passenger passenger : passengers) {
@@ -40,13 +42,18 @@ public class PassengersService {
     return result;
   }
 
-  public void deleteAllPassengersFromTrip(long id) {
+  public boolean deleteAllPassengersFromTrip(long id) {
     Iterable<Passenger> passengers = repository.findByTripId(id);
+    if(passengers == null)
+      return false;
     repository.deleteAll(passengers);
+    return true;
   }
 
   public Trips readTripByPassengerId(long id) {
     Iterable<Passenger> passengers = repository.findByUserId(id);
+    if (passengers == null)
+      return null;
     Trips result = new Trips();
     for (Passenger passenger : passengers ) {
       Trip trip = tripProxy.readOne(passenger.getTripId());
@@ -59,18 +66,25 @@ public class PassengersService {
     return result;
   }
 
-  public void deletePassengersFromTrip(long id) {
+  public boolean deletePassengersFromTrip(long id) {
     Iterable<Passenger> passengers = repository.findByUserId(id);
+    if(passengers == null)
+      return false;
     repository.deleteAll(passengers);
+    return true;
   }
 
-  public void addPassengerToTrip(long userId, long tripId) {
+  public boolean addPassengerToTrip(long userId, long tripId) {
     Passenger newPassenger = new Passenger();
+    Passenger verification = repository.findByTripIdAndUserId(userId,tripId);
+    if(verification==null)
+      return false;
     newPassenger.setStatus("pending");
     newPassenger.setTripId((int) tripId);
     newPassenger.setUserId((int) userId);
 
     repository.save(newPassenger);
+    return true;
   }
 
   public String readStatusFromPassenger(long userId, long tripId) {
@@ -78,15 +92,21 @@ public class PassengersService {
     return repository.findByTripIdAndUserId(userId,tripId).getStatus();
   }
 
-  public void updatePassengerStatus(long tripId, long userId, String status) {
+  public boolean updatePassengerStatus(long tripId, long userId, String status) {
 
     Passenger passenger = repository.findByTripIdAndUserId(tripId,userId);
+    if(passenger == null)
+      return false;
     passenger.setStatus(status);
     repository.save(passenger);
+    return true;
   }
 
-  public void deletePassengerFromTrip(long tripId, long userId) {
+  public boolean deletePassengerFromTrip(long tripId, long userId) {
     Passenger passenger = repository.findByTripIdAndUserId(tripId,userId);
+    if(passenger == null)
+      return false;
     repository.delete(passenger);
+    return true;
   }
 }
